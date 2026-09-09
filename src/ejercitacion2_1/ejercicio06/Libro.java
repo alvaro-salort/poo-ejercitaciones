a29e5244f72a52b757347a06c0433cb79c39b350
alvaro-salort <salort.alvaro@uncuyo.edu.ar>
2026-09-09 20:58:37 -0300

    Iniciado ejercicio 6

---
+++ src/ejercitacion2_1/ejercicio06/Libro.java
@@ -0,0 +1,24 @@
+package ejercitacion2_1.ejercicio06;
+
+public class Libro {
+
+    String titulo;
+    String autor;
+    int paginasTotales;
+    int paginaActual;
+
+    public Libro(String titulo, String autor, int paginasTotales) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.paginasTotales = paginasTotales;
+        this.paginaActual = 0;
+    }
+    
+
+
+    public static void main(String[] args){
+
+    }
+
+
+}
