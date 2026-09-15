1279f25041a283e1299e342240b3ee96bc3e0527
alvaro-salort <salort.alvaro@uncuyo.edu.ar>
2026-09-15 09:08:49 -0300

    Fix visual del resultado en consola del Ejercicio2_1_06

--- src/ejercitacion2_1/ejercicio06/Libro.java
+++ src/ejercitacion2_1/ejercicio06/Libro.java
@@ -24,23 +24,29 @@ public class Libro {
 
     public double mostrarProgreso(){
         double progreso = paginaActual / (double) paginasTotales * 100;
-        System.out.printf("Libro: %-20s | Autor: %-20s | progreso: %.2f%% %n", titulo, autor, progreso);
+        System.out.printf("Libro: %-25s | Autor: %-20s | progreso: %.2f%% %n", titulo, autor, progreso);
         return progreso;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args){ 
 
         Libro libro1 = new Libro("El Señor de los Anillos", "J.R.R. Tolkien", 1000);
         libro1.paginaActual = 570;
         libro1.mostrarProgreso();
         libro1.leer(3);
-        System.out.println("Leyendo tres páginas...");
+
+        System.out.println("Leyendo 500 paginas...");
+        libro1.leer(500);
         libro1.mostrarProgreso();
+
+        System.out.println("\n");
 
         Libro libro2 = new Libro("1984", "George Orwell", 328);
         libro2.paginaActual = 100;
         libro2.mostrarProgreso();
+        System.out.println("Leyendo 12 páginas");
+        libro2.leer(12);
+        libro2.mostrarProgreso();
     }
-
 
 }
